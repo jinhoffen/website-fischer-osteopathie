357b77f32aa31246f2bb95b80ce83d073651421e
Justus Inhoffen <jinhoffen@tinbergen.nl>
2023-05-03 01:48:17 +0200

    add info box to maps icon

--- js/google_maps.js
+++ js/google_maps.js
@@ -8,38 +8,37 @@ function initMap() {
         zoom: 11,
         center: { lat: 52.4973063108, lng: 13.3621763999 },
         streetViewControl: false,
-        mapTypeControl: false
+        mapTypeControl: false,
+        keyboardShortcuts: false
     });
-    setMarkers(map);
-}
 
-// data for markers
-const locations = [
-    [
-        "{{ site.data.global.contact.praxis_berlin_mitte.name }}",
-        {{ site.data.global.contact.praxis_berlin_mitte.lat }},
-        {{ site.data.global.contact.praxis_berlin_mitte.long }}
-        
-    ],
-    [
-        "{{ site.data.global.contact.praxis_berlin_steglitz.name }}",
-        {{ site.data.global.contact.praxis_berlin_steglitz.lat }},
-        {{ site.data.global.contact.praxis_berlin_steglitz.long }}
-    ]
-];
+    // data for markers
+    const locations = [
+        [{ lat: {{ site.data.global.contact.praxis_berlin_mitte.lat }}, lng: {{ site.data.global.contact.praxis_berlin_mitte.long }} }, '<a href="{{ site.data.global.contact.praxis_berlin_mitte.maps }}" rel="noopener" target="_blank">{{ site.data.global.contact.praxis_berlin_mitte.name }}</a>'],
+        [{ lat: {{ site.data.global.contact.praxis_berlin_steglitz.lat }}, lng: {{ site.data.global.contact.praxis_berlin_steglitz.long }} }, '<a href="{{ site.data.global.contact.praxis_berlin_steglitz.maps }}" rel="noopener" target="_blank">{{ site.data.global.contact.praxis_berlin_steglitz.name }}</a>']
+    ];
 
-function setMarkers(map) {
-    // define shapes of clickable icon
-    for (let i = 0; i < locations.length; i++) {
-        const location = locations[i];
+    // create an info window to share between markers.
+    const infoWindow = new google.maps.InfoWindow();
 
-        new google.maps.Marker({
-            position: { lat: location[1], lng: location[2] },
+    // Create the markers.
+    locations.forEach(([position, title], i) => {
+        const marker = new google.maps.Marker({
+            position,
             map,
-            title: location[0],
-            zIndex: location[3],
+            title: `${i + 1}. ${title}`,
+            label: `${i + 1}`,
+            optimized: false,
         });
-    }
+
+        // Add a click listener for each marker, and set up the info window.
+        marker.addListener("click", () => {
+            infoWindow.close();
+            infoWindow.setContent(marker.getTitle());
+            infoWindow.open(marker.getMap(), marker);
+        });
+    });
+
 }
 
 console.log("google-maps-loaded");
